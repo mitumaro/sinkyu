9e3eaa6e692c066f07912ae35af1f2743320e819
Isikawa <24aw0107@jec.ac.jp>
2024-12-15 15:55:39 +0900

    12/15

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "./components/Header";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "../index.css"; // Ensure the CSS file path is correct.
 
 const Header = () => {
